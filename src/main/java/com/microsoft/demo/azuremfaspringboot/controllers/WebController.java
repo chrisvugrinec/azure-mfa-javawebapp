f4b5596e627a28eb963ff9aed2e938245e776ec2
gogsadmin <RedHat$1>
2017-10-29 11:33:26 +0100

    added openshift

--- src/main/java/com/microsoft/demo/azuremfaspringboot/controllers/WebController.java
+++ src/main/java/com/microsoft/demo/azuremfaspringboot/controllers/WebController.java
@@ -3,6 +3,7 @@ package com.microsoft.demo.azuremfaspringboot.controllers;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class WebController {
 
+//	@PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_ADMIN')")
 	@RequestMapping(value = "/hello", method = { RequestMethod.GET, RequestMethod.POST })
 	public String defaultPage(ModelMap model, HttpServletRequest httpRequest) {
 		return "hello";
